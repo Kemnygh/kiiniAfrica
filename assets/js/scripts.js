ea127ee36207dd6b41a02120774b257701c0ded2
Kemuel Nalifuma <kemnygh@gmail.com>
2021-11-14 17:16:12 +0300

    Style data collection side of main page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
 // prompt("name")
 
-var imageparent = document.querySelector("#main-left");
+var imageparent = document.querySelector("#img-box");
 var m_image = document.createElement('img');
 imageparent.appendChild(m_image);
+m_image.classList.add("gnd-img")
 // var femaleIcon = m_image.src = "./assets/images/female.g.png";
 var neutralIcon = m_image.src = "./assets/images/neutral.g.png";
 
@@ -16,3 +17,8 @@ fe_icon.addEventListener('click',function(event){
 ma_icon.addEventListener('click',function(event){
   m_image.src = "./assets/images/male.g.png";
 })
+
+m_image.style.marginTop = "50px";
+m_image.style.marginLeft = "5%";
+m_image.style.maxWidth = "100%";
+m_image.style.maxHeight = "300px";
